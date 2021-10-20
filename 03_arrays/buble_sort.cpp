8a474cd2776a612205c338079a335d7a16dcaf18
anton0xf <anton0xf@gmail.com>
2021-10-20 09:49:35 +0300

    03_arrays/buble_sort.cpp: fix warning

--- 03_arrays/buble_sort.cpp
+++ 03_arrays/buble_sort.cpp
@@ -9,7 +9,8 @@ void print_a(int n, int a[]) {
 }
 
 int main() {
-  int n = 4; int a[n] = {4, 3, 2, 1};
+  const int n = 4;
+  int a[n] = {4, 3, 2, 1};
   for (int i=n-1; i>0; i--) {
     // cout << "i = " << i << endl;
     for (int j=0; j<i; j++) {
